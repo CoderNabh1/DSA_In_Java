93ffd86e3ed39fdce1a9eb4e866eb509d567d38b
Nabh Garg <nabhgargdholpur@gmail.com>
2026-03-02 22:43:23 +0530

    Just Starting DP and had learn about memorization concept.     1. Had made a global array dp --> to store the result of the subproblems already calculated.

---
+++ DP/FibonaciSequence/Fibonaci_Memoization.java
@@ -0,0 +1,32 @@
+package FibonaciSequence;
+
+import java.util.Scanner;
+
+public class Fibonaci_Memoization {
+    // Static array shared by all calls
+    static int[] dp;
+
+    public static int fibonaci(int n) {
+        if (n <= 1)
+            return n;
+
+        // Check the global array
+        if (dp[n] != 0)
+            return dp[n];
+
+        dp[n] = fibonaci(n - 1) + fibonaci(n - 2);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        // Initialize the global array based on user input
+        dp = new int[n + 1];
+
+        System.out.println("Result: " + fibonaci(n));
+        sc.close();
+    }
+}
